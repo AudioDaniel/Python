c59aea1d6ef14d24c30d1ce1e0569d06f5fbceb7
Audionoob <danielmvazquez.audio@gmail.com>
2022-09-01 20:09:25 +0200

    Test

---
+++ Areas.py
@@ -0,0 +1,15 @@
+def calc_area_triangulo():
+    area_triangulo = (base*altura)/2
+    print(area_triangulo)
+
+altura = 5
+base = 5
+
+calc_area_triangulo()
+
+def calc_area_circulo():
+    area_circulo = (3.1415 * radio**2)
+    print(area_circulo)
+
+radio = 5
+calc_area_circulo()
